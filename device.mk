edf71653f18d5df61cabe27b692e58f7ab939eb9
saku-bruh <saku-bruh@proton.me>
2024-06-01 18:51:19 +0300

    houji: Adapt
    * Do the following in the ROM directory:
    
    git clone -b uvite https://github.com/AOSPA/android_hardware_xiaomi hardware/xiaomi
    rmz hardware/nxp/keymint && rmz hardware/nxp/nfc && rmz hardware/nxp/secure_element && rmz hardware/nxp/weaver
    git clone -b br_android_ncihalx_comm_15 https://github.com/NXPNFCProject/nfcandroid_keymaster_hidlimpl hardware/nxp/keymaster
    git clone -b br_android_ncihalx_comm_15 https://github.com/NXPNFCProject/nfcandroid_keymint_hidlimpl hardware/nxp/keymint
    git clone -b br_android_ncihalx_comm_15 https://github.com/NXPNFCProject/nfcandroid_nfc_hidlimpl hardware/nxp/nfc
    git clone -b br_android_ncihalx_comm_15 https://github.com/NXPNFCProject/nfcandroid_secureelement hardware/nxp/secure_element
    git clone -b br_android_ncihalx_comm_15 https://github.com/NXPNFCProject/nfcandroid_nxp_ese_clients/ hardware/nxp/secure_element_extns
    git clone -b br_android_ncihalx_comm_15 https://github.com/NXPNFCProject/nfcandroid_weaver_hidlimpl hardware/nxp/weaver
    cd hardware/qcom-caf && cpz sm8550 sm8650 && cd sm8650 && rmz * && ln -s ../sm8550/Android.mk Android.mk && ln -s ../sm8550/Android.bp Android.bp && git clone https://github.com/OnePlus-12-Development/android_hardware_qcom_media media && git clone https://github.com/OnePlus-12-Development/android_vendor_qcom_opensource_data-ipa-cfg-mgr data-ipa-cfg-mgr && git clone https://github.com/OnePlus-12-Development/android_vendor_qcom_opensource_dataipa dataipa && git clone https://github.com/OnePlus-12-Development/android_hardware_qcom_display display && git clone https://github.com/OnePlus-12-Development/android_hardware_qcom_audio-ar audio && cd ../../..
    
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- device.mk
+++ device.mk
@@ -372,7 +372,8 @@ PRODUCT_PACKAGES += \
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/qcom-caf/sm8650
 
 # Telephony
 PRODUCT_COPY_FILES += \
